384608642d1628bc92b619ddff91fac57afe981d
Panji Kusuma <epanji@gmail.com>
2017-08-22 16:25:53 +0700

    tcgci now depends on ci-core.

--- qzuma-tcgci.el
+++ qzuma-tcgci.el
@@ -24,131 +24,35 @@
 
 ;;; Code:
 
-(require 'qzuma-core)
-
-(qz-define-field-type
- "qz-tcgci" "picture" "type=\"file\""
-
- ;; english
-
- "icon[s]?$"
- "image[s]?$"
- "logo[s]?$"
- "photo[s]?$"
- "picture[s]?$"
-
- ;; indonesia
-
- "foto$"
- "gambar$"
- "ikon$")
-
-(qz-define-field-type
- "qz-tcgci" "audio" "type=\"file\""
-
- ;; english
-
- "audio[s]?$"
- "music[s]?$"
- "sound[s]?$"
-
- ;; indonesia
-
- "lagu$"
- "musik$"
- "suara$")
-
-(qz-define-field-type
- "qz-tcgci" "video" "type=\"file\""
-
- ;; english
-
- "clip[s]?$"
- "movie[s]?$"
- "video[s]?$"
-
- ;; indonesia
-
- "film$"
- "klip$"
- "vidio$")
-
-(qz-define-field-type
- "qz-tcgci" "file" "type=\"file\""
-
- ;; english
-
- "file[s]?$"
- "image[s]?$"
-
- ;; indonesia
-
- "berkas$")
-
-(setq qz-tcgci-file
-      (append qz-tcgci-file
-              qz-tcgci-picture
-              qz-tcgci-audio
-              qz-tcgci-video))
+(require 'qzuma-ci-core)
 
 ;; functions
 
-(defun qz-tcgci-class-controller (name)
-  "Create class controller."
-  (concat
-   (qz-line 0 1 "<?php")
-   (qz-line 0 0 "defined('BASEPATH') OR ")
-   (qz-line 0 2 "exit('No direct script access allowed');")
-   (qz-line 0 0 (format "class %s " name))
-   (qz-line 0 1 "extends CI_Controller")
-   (qz-line 0 1 "{")
-   (qz-line 1 1 "public function __construct() {")
-   (qz-line 2 1 "parent::__construct();")
-   (qz-line 1 2 "}")
-   (qz-line 0 0 "}")))
-
-(defun qz-tcgci-function-controller (fields name &optional controller)
-  "Create function from table fields inside controller."
+(defun qz-tcgci-method (fields name &optional class type)
+  "Create function from table fields inside class."
   (if (qz-table-p fields)
       (progn
-        (unless controller
-          (setq controller "Controller"))
-        (qz-open-continue-buffer (concat controller ".php"))
+        (unless class
+          (setq class "Debug"))
+        (unless type
+          (setq type "global"))
+        (qz-open-continue-buffer (concat class ".php"))
         (when (equal 1 (point-at-bol))
-          (insert (qz-tcgci-class-controller controller))
+          (insert (qz-ci-class-wrapper name type))
           (forward-line -1))
         (when (fboundp 'web-mode) (web-mode))
         (insert
          (qz-line 0 1 "")
          (qz-line 1 1 (format "public function %s() {" name))
          (qz-line 2 2 "$data = array();")
-
+         
          ;; content get / set or if-get
          ;; (qz-function-contents fields 2)
 
          (qz-line 0 1 "")
-         (qz-line 2 0 "$this->load->view('")
-         (qz-line 0 0 (downcase (concat controller "_" name)))
-         (qz-line 0 1 "', $data);")
+         (qz-ci-method-footer type class name)
          (qz-line 1 0 "}")))
     (print "Selected region not well formatted")))
-
-(defun qz-tcgci-class-model (name)
-  "Create class models."
-  (concat
-   (qz-line 0 1 "<?php")
-   (qz-line 0 0 "defined('BASEPATH') OR ")
-   (qz-line 0 2 "exit('No direct script access allowed');")
-   (qz-line 0 0 (format "class %s_model " name))
-   (qz-line 0 1 "extends CI_Model")
-   (qz-line 0 1 "{")
-   (qz-line 1 1 "public function __construct() {")
-   (qz-line 2 1 "parent::__construct();")
-   (qz-line 0 1 "$this->load->library('pagination');")
-   (qz-line 0 1 "$this->load->library('form_validation');")
-   (qz-line 0 1 "$this->load->library('upload');")
-   (qz-line 1 2 "}")
-   (qz-line 0 0 "}")))
 
 ;; commands
 
@@ -159,16 +63,36 @@
 	  (let ((fields (qz-list-from-region
                      (region-beginning)
                      (region-end)))
-			(controller (qz-upper-first
-                         (read-from-minibuffer "Controller name: "))))
+            (type "controller")
+			(controller (qz-upper-first (read-from-minibuffer
+                                         "Controller name: "))))
 		(if (qz-table-p fields)
 			(progn
-              (qz-open-clear-buffer (concat controller ".php"))
-              (insert (qz-tcgci-class-controller controller))
+              (qz-open-clear-buffer (format "%s.php" controller))
+              (insert (qz-ci-class-wrapper controller type))
               (qz-open-continue-buffer (concat controller ".php"))
-              (qz-tcgci-function-controller fields "index" controller)
-              (qz-tcgci-function-controller fields "data" controller)
+              (qz-tcgci-method fields "index" controller type)
+              ;; ok
+              )
+		  (print "Selected region not well formatted")))
+	(print "No region selected")))
 
+(defun qz-tcgci-create-model ()
+  "Create model from table fields in region."
+  (interactive)
+  (if (use-region-p)
+	  (let ((fields (qz-list-from-region
+                     (region-beginning)
+                     (region-end)))
+            (type "model")
+			(model (qz-upper-first (read-from-minibuffer
+                                    "Model name: "))))
+		(if (qz-table-p fields)
+			(progn
+              (qz-open-clear-buffer (format "%s_model.php" model))
+              (insert (qz-ci-class-wrapper model type))
+              (qz-open-continue-buffer (format "%s_model.php" model))
+              (qz-tcgci-method fields "index" (format "%s_model" model))
               ;; ok
               )
 		  (print "Selected region not well formatted")))
